d7f766009160ad2cb02707c3fdd427b73cfbc477
Just Nyde <88275041+justnyde@users.noreply.github.com>
2022-09-27 23:43:29 +0300

    Responsive.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ export default function Footer(){
     <div align="center">
         <div className="w-9/12 space-y-6">
             <hr className="divider w-full"></hr>
-            <div className="flex justify-between items-center">
+            <div className="block space-y-4 sm:space-y-0 sm:flex sm:justify-between sm:items-center">
                 <h1 className="font-sans text-gray-900 dark:text-white text-base">Copyright © 2022</h1>
                 <h1 className="font-sans text-gray-900 dark:text-white text-base flex gap-2 items-center">Made with <SiReact color="AQUA"/> React by <a href={`https://github.com/${UserData.github}`}>Just Nyde</a></h1>
             </div>
