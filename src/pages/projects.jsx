fbc802a0adb20a6a263102d27fd9897eb394e7a2
Just Nyde <88275041+justnyde@users.noreply.github.com>
2022-09-28 17:14:19 +0300

    Update projects.jsx

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,5 +1,8 @@
 import Projects from "../data/projects"
 import { useState, useEffect } from "react"
+import { TbHeartHandshake } from "react-icons/tb"
+import { SiDiscord } from "react-icons/si"
+import UserData from "../data/user.js"
 
 export default function Project(){
 
@@ -15,7 +18,7 @@ export default function Project(){
 
     return (
         <div className="mt-3 max-w-8xl w-11/12 sm:w-8/12 mx-auto">
-            <div className="font-bold font-sans text-slate-900 dark:text-white text-4xl">Projects</div>
+            <h1 className="font-bold font-sans text-slate-900 dark:text-white text-4xl">Projects</h1>
             <br/>
             {(!projects) ? (<>
             </>) : ((projects.length == 0) ? (<></>) : (<>
@@ -32,6 +35,29 @@ export default function Project(){
                     ))}
                 </div>
             </>))}
+            <br/><br/>
+
+            <div className="flex items-center gap-4">
+                <div className="p-4 rounded-full bg-rose-400 dark:bg-gray-800 hover:bg-rose-600-400 hover:border-red-900 border-2 border-solid border-red-600 transition">
+                    <TbHeartHandshake className="text-black dark:text-white" size={"25px"}/>
+                </div>
+                <h1 className="text-2xl text-gray-900 dark:text-white font-bold font-sans">Work with me.</h1>
+            </div>
+
+            <br/>
+
+            <a href={UserData.connections.discord.server}>
+                <div className="block bg-gradient-to-r from-[#4C62EF] to-[#374ac2] w-full sm:w-4/12 p-5 rounded-lg group">
+                    <div className="flex items-center gap-4 w-full">
+                        <div className="w-16 h-16 flex items-center justify-center bg-[rgba(0,0,0,0.40)] rounded-lg border-2 border-solid border-white"><SiDiscord className="group-hover:scale-125 transition" color="WHITE" size="25px"/></div>
+                        <div>
+                            <h1 className="font-sans text-white text-lg">Discord</h1>
+                            <h1 className="font-sans text-gray-200 text-base">{UserData.connections.discord.name}</h1>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
         </div>
     )
 
