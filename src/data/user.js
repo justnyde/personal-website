c91cad7d81903a036b5bdf99dd6950b924ad79d1
Just Nyde <88275041+justnyde@users.noreply.github.com>
2022-11-30 18:10:29 +0300

    Create user.js

---
+++ src/data/user.js
@@ -0,0 +1,32 @@
+export default {
+    userId: "642752306441617417", //You should replace it to your discord ID.
+    description: "I am 16 years old and I'm a Turkish student. I've been coding since 2019. I want to develop and grow myself and learn more about everything. I can build websites.",
+    github: "justnyde",
+    education: {
+        primary: {
+            name: "Mareşal Fevzi Çakmak",
+            location: "Istanbul, Küçükçekmece"
+        },
+        secondary: {
+            name: "Mareşal Fevzi Çakmak",
+            location: "Istanbul, Küçükçekmece"
+        },
+        high: {
+            name: "Kadriye Moroğlu",
+            location: "Istanbul, Küçükçekmece"
+        },
+        university: {
+            name: "SOON :)",
+            location: "Istanbul (I hope :))"
+        }
+    },
+    connections: {
+        youtube: "Just Nyde",
+        instagram: "@just.nyde",
+        twitter: "Just Nyde",
+        discord: {
+            name: "Just Nyde#2535",
+            server: "https://discord.gg/xhZnrsE5Cm"
+        }
+    }
+}
