a15188f6c84f0cf7fd8e62ee65dc0630cdacff58
Just Nyde <88275041+justnyde@users.noreply.github.com>
2022-11-30 18:09:57 +0300

    Create projects.jsx

---
+++ src/data/projects.jsx
@@ -0,0 +1,17 @@
+export default {
+
+    personal: {
+        name: "Personal Website",
+        description: "My personal website for introduction my career and my life.",
+        thumbnail: "/image/projects/personalweb.png",
+        link: "https://nyde.online/"
+    },
+    
+    botscase: {
+        name: "Bots Case #SOON",
+        description: "Find the best discord bot for your server in BOTS CASE and upgrade your discord experience. WEBSITE WILL BE OPENED #SOON.",
+        thumbnail: "/image/projects/botscase.png",
+        link: "https://discord.gg/wJjHc2VRRR"
+    }
+
+}
